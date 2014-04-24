f57c4817d058fbdff9440e32611d28ca692c94e1
Jacqueline Leykam <jleykam@gmail.com>
2014-04-25 09:31:15 +1000

    Enable tests and add tests for marker filters.

---
+++ test/SmoothingMarkerFilterTest.cpp
@@ -0,0 +1,60 @@
+#include "gtest/gtest.h"
+
+#include <vector>
+#include <aruco/aruco.h>
+
+#include "filter.h"
+
+class SmoothingMarkerFilterTest : public ::testing::Test
+{
+protected:
+		void AddMarkerToVector(int id) {
+			aruco::Marker marker;
+			marker.id = id;
+			markers_.push_back(marker);
+		}
+
+		std::vector<aruco::Marker> markers_;
+};
+
+TEST_F(SmoothingMarkerFilterTest, RemembersMarker) {
+	SmoothingMarkerFilter filter(1);
+
+	AddMarkerToVector(0);
+	filter.Filter(&markers_);
+
+	// First filter should not touch the markers.
+	ASSERT_EQ(1, markers_.size());
+	ASSERT_EQ(0, markers_[0].id);
+
+	markers_.clear();
+	ASSERT_EQ(0, markers_.size());
+	filter.Filter(&markers_);
+
+	// Second filter should add the missing marker.
+	ASSERT_EQ(1, markers_.size());
+	ASSERT_EQ(0, markers_[0].id);
+}
+
+TEST_F(SmoothingMarkerFilterTest, ForgetsMarker) {
+	SmoothingMarkerFilter filter(1);
+
+	AddMarkerToVector(0);
+	filter.Filter(&markers_);
+
+	// First filter should not touch the markers.
+	ASSERT_EQ(1, markers_.size());
+	ASSERT_EQ(0, markers_[0].id);
+
+	markers_.clear();
+	ASSERT_EQ(0, markers_.size());
+	filter.Filter(&markers_);
+
+	markers_.clear();
+	ASSERT_EQ(0, markers_.size());
+	filter.Filter(&markers_);
+
+	// Third filter should have forgotten the marker.
+	ASSERT_EQ(0, markers_.size());
+}
+
