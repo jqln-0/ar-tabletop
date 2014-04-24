249f27fafdeeb19689a0ceae288d935cdb0bc52a
Jacqueline Leykam <jleykam@gmail.com>
2014-04-24 17:26:02 +1000

    Add a function to convert OpenCV Mats to QImages.

---
+++ src/matconv.cpp
@@ -0,0 +1,11 @@
+#include "matconv.h"
+
+QImage matToQImage(cv::Mat m) {
+  // Convert the Mat to RGB colorspace.
+  cv::Mat conv;
+  cv::cvtColor(m, conv, cv::COLOR_BGR2RGB);
+
+  // Construct and return the QImage.
+  return QImage((unsigned char*)conv.data, conv.cols, conv.rows,
+                QImage::Format_RGB888);
+}
---
+++ src/matconv.h
@@ -0,0 +1,9 @@
+#ifndef __MATCONV_H_
+#define __MATCONV_H_
+
+#include <QImage>
+#include <aruco/aruco.h>
+
+QImage matToQImage(cv::Mat m);
+
+#endif  // __MATCONV_H_
