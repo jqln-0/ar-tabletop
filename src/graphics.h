3f53673b4909dc68a479d089f8b8ea754616b966
Jacqueline Leykam <jleykam@gmail.com>
2014-05-18 11:09:54 +1000

    Minor cleanup of OpenGL widget.
    Draw placeholder cubes over markers *should* now work.
    It's completely untested though since we can't calibrate yet.

--- src/graphics.h
+++ src/graphics.h
@@ -7,26 +7,26 @@
 #include <vector>
 
 class SceneWidget : public QGLWidget {
-	Q_OBJECT
-	protected:
-		// OpenGL methods for scene drawing.
-		void initializeGL();
-		void paintGL();
-		void resizeGL(int width, int height);
+  Q_OBJECT
+ protected:
+  // OpenGL methods for scene drawing.
+  void initializeGL();
+  void paintGL();
+  void resizeGL(int width, int height);
 
-		// Scene data.
-		aruco::CameraParameters camera_;
+  // Scene data.
+  aruco::CameraParameters camera_;
 
-		// Marker and object data.
-		std::vector<aruco::Marker> markers_;
+  // Marker and object data.
+  std::vector<aruco::Marker> markers_;
 
-	public:
-		explicit SceneWidget(QWidget *parent = nullptr);
+ public:
+  explicit SceneWidget(QWidget *parent = nullptr);
 
-		void set_markers(const std::vector<aruco::Marker> &markers);
-		void set_camera(const aruco::CameraParameters &camera);
+  void set_markers(const std::vector<aruco::Marker> &markers);
+  void set_camera(const aruco::CameraParameters &camera);
 
-		void DrawCube();
+  void DrawCube();
 };
 
-#endif // __GRAPHICS_H_
+#endif  // __GRAPHICS_H_
